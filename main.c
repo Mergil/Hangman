ed6c5fc5a9330feb83f9e235b076fe90a1cdb5f5
Баротов Руслан Николаевич <ri68s02@lwp12.csc.local>
2017-05-18 12:01:45 +0700

    Version 1.0 Alpha (Work with main function and Add book's words file for Hangman

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+void igra(char *zagadka);
+
+int main() {
+setlocale(LC_ALL,"rus");
+int i=0;
+srand(time(0));
+char *zagadka=malloc(sizeof(char*)*100);
+FILE *myfile;
+if ((myfile=fopen("book.txt","r"))) {
+while (fscanf(myfile,"%s",zagadka) != EOF) i++;
+if (i>0) {
+int j=rand()%i;
+fseek(myfile,0,SEEK_SET); /// В начало документа
+for (i=0;i<=j;i++) fscanf(myfile,"%s",zagadka);
+zagadka=realloc(zagadka,sizeof(char*)*strlen(zagadka)); /// Выделение новой памяти
+} else i=-1;
+} else myfile=fopen("book.txt","w");
+fclose(myfile);
